f11b07ec3b5373147af929ea8513a78f2ae3510c
Gil Vegliach <gil.vegliach@gmail.com>
2016-02-18 20:29:43 +0100

    Fix server response header

--- server/src/main/java/it/gilvegliach/learning/networkdexloading/DexController.java
+++ server/src/main/java/it/gilvegliach/learning/networkdexloading/DexController.java
@@ -2,6 +2,10 @@ package it.gilvegliach.learning.networkdexloading;
 
 import com.google.common.io.ByteStreams;
 import com.google.common.io.Resources;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,9 +17,13 @@ import java.net.URL;
 @RestController
 public class DexController {
 
-    @RequestMapping(value = "/{file:.+}", method = RequestMethod.GET, produces = "application/octet-stream")
-    public byte[] getDex(@PathVariable String file) throws IOException {
+    @RequestMapping(value = "/{file:.+}", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getDex(@PathVariable String file) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", file);
         URL res = Resources.getResource(file);
-        return ByteStreams.toByteArray(res.openStream()); // Content-Disposition is still wrong
+        byte[] data = ByteStreams.toByteArray(res.openStream());
+        return new ResponseEntity<>(data, headers, HttpStatus.OK);
     }
 }
